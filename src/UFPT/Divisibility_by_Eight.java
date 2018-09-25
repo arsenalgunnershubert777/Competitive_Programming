d75a15a7344010024e352ce2e14a437dc49289b4
Hubert Zhao <hubert777@ufl.edu>
2018-09-25 00:47:20 -0400

    some more problems

---
+++ src/UFPT/Divisibility_by_Eight.java
@@ -0,0 +1,35 @@
+package UFPT;
+import java.util.*;
+
+public class Divisibility_by_Eight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String num = sc.next();
+        boolean divisible = true;
+        for (int i = 0; i < 125; i++) {
+            String eight = Integer.toString((i*8));
+            int currIndex = 0;
+            divisible = true;
+            for (int j = 0; j < eight.length(); j++) {
+                int test = num.indexOf(eight.charAt(j), currIndex);
+                if (test != -1) {
+                    currIndex = test+1;
+                }
+                else {
+                    divisible = false;
+                    break;
+                }
+            }
+            if (divisible) {
+                System.out.println("YES");
+                System.out.println(eight);
+                break;
+            }
+
+        }
+        if (!divisible) {
+            System.out.println("NO");
+        }
+
+    }
+}
