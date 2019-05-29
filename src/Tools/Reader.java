106dd35c24132aa8acebba136a1e047a5b0d941d
Hubert Zhao <hubert777@ufl.edu>
2019-05-29 15:40:06 -0400

    some more problems

---
+++ src/Tools/Reader.java
@@ -0,0 +1,71 @@
+package Tools;
+//from https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/ and https://github.com/cormacpayne
+import java.io.DataInputStream;
+import java.io.InputStream;
+
+class Reader{
+    final private int BUFFER_SIZE = 1 << 16;
+
+    private DataInputStream din;
+    private byte[] buffer;
+    private int bufferPointer, bytesRead;
+
+    public Reader(InputStream in){
+        din = new DataInputStream(in);
+        buffer = new byte[BUFFER_SIZE];
+        bufferPointer = bytesRead = 0;
+    }
+
+    public int nextInt() throws Exception{
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = c == '-';
+        if (neg) c = read();
+        do
+        {
+            ret = ret * 10 + c - '0';
+            c = read();
+        } while (c > ' ');
+        if (neg) return -ret;
+        return ret;
+    }
+
+    public String readLine() throws Exception
+    {
+        byte[] buf = new byte[64]; // line length
+        int cnt = 0, c;
+        while ((c = read()) != -1)
+        {
+            if (c == '\n')
+                break;
+            buf[cnt++] = (byte) c;
+        }
+        return new String(buf, 0, cnt);
+    }
+
+
+    //reads in the next string
+    public String next() throws Exception
+    {
+        StringBuilder ret = new StringBuilder();
+        byte c = read();
+        while (c <= ' ') c = read();
+        do
+        {
+            ret = ret.append((char)c);
+            c = read();
+        } while (c > ' ');
+        return ret.toString();
+    }
+
+    private void fillBuffer() throws Exception{
+        bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+        if (bytesRead == -1) buffer[0] = -1;
+    }
+
+    private byte read() throws Exception{
+        if (bufferPointer == bytesRead) fillBuffer();
+        return buffer[bufferPointer++];
+    }
+}
